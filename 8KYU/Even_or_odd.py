21bad8aa3d0c864c61eb2bd763501d7c948dc5c2
h4ns <88755387+h4ns21@users.noreply.github.com>
2021-10-02 23:18:33 +0200

    x

---
+++ 8KYU/Even_or_odd.py
@@ -0,0 +1,11 @@
+# First method
+def even_or_odd(n):
+    if (n % 2) == 0:
+        result="Even"
+    else:
+        result="Odd"
+    return result
+
+# Second method
+def even_or_odd(number):
+    return 'Odd' if number % 2 else 'Even'
