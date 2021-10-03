ee7bc344f76d89dd427c60216789f35d8db7bfde
h4ns <88755387+h4ns21@users.noreply.github.com>
2021-10-03 21:02:31 +0200

    x

--- 8KYU/Find_Multiples_of_a_Number.py
+++ 8KYU/Find_Multiples_of_a_Number.py
@@ -1,3 +1,8 @@
+# First method
 def find_multiples(integer, limit):
     res = [i for i in range(integer, limit+1, integer)] 
     return res
+
+# Second method
+def find_multiples(integer, limit):
+    return list(range(integer, limit+1, integer))
