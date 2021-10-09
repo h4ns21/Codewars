34879598f52f4fd7933e3ef73c099b35eecb918d
h4ns <88755387+h4ns21@users.noreply.github.com>
2021-10-09 12:38:58 +0200

    x

--- 8KYU/Multiplication_table_for_number.py
+++ 8KYU/Multiplication_table_for_number.py
@@ -1,5 +1,10 @@
+# First method
 def multi_table(n):
     out = ""
-    for c in range(1,11):
-        out += (str(c) + ' * ' + str(n) + ' = ' + str(n*c) + "\n")
+    for i in range(1,11):
+        out += (str(i) + ' * ' + str(n) + ' = ' + str(n*c) + "\n")
     return out.strip()
+
+# Second method
+def multi_table(n):
+    return '\n'.join(f'{i} * {n} = {i * n}' for i in range(1, 11))
