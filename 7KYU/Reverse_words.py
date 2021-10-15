15559b193dd90dc3e136ad74fc61bd297a5e296f
h4ns <88755387+h4ns21@users.noreply.github.com>
2021-10-15 09:26:11 +0200

    x

---
+++ 7KYU/Reverse_words.py
@@ -0,0 +1,10 @@
+# First method
+def reverse_words(text):
+    words = text.split(" ")
+    newWords = [word[::-1] for word in words]
+    newSentence = " ".join(newWords)
+    return newSentence
+
+# Second method
+def reverse_words(text):
+    return ' '.join(word[::-1] for word in text.split(" "))
