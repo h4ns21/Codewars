187480031579afa1b4e3921d10613762edac1a60
h4ns <88755387+h4ns21@users.noreply.github.com>
2021-10-19 12:31:29 +0200

    x

--- 8KYU/Thinkful_-_Logic_Drills:_Traffic_light.py
+++ 8KYU/Thinkful_-_Logic_Drills:_Traffic_light.py
@@ -1,2 +1,7 @@
+# First method
 def update_light(current):
     return 'yellow' if current == 'green' else 'green' if current == 'red' else 'red'
+
+#Second method
+def update_light(current):
+    return {"green": "yellow", "yellow": "red", "red": "green"}[current]
