b8deebd04490cc44a0e25db12971d60f165fd085
h4ns <88755387+h4ns21@users.noreply.github.com>
2021-10-19 11:59:08 +0200

    x

---
+++ 8KYU/Double_Char.c
@@ -0,0 +1,10 @@
+/* First method */
+def double_char(s):
+    outs = ''
+    for i in s:
+        outs = outs + i + i
+    return outs
+    
+/* Second method */
+def double_char(s):
+    return ''.join([c+c for c in s])  /* (c*2 for c in s) */
