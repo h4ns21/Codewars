e6749775639836dab368b48fc58927f700fe4bd9
H4ns <88755387+h4ns21@users.noreply.github.com>
2022-01-27 19:31:38 +0100

    x

---
+++ 7KYU/Move_10.py
@@ -0,0 +1,16 @@
+# First method
+def move_ten(st): 
+    cipherText = ""
+    for ch in st:
+        if ch.isalpha():
+            stayInAlphabet = ord(ch) + 10 
+            if stayInAlphabet > ord('z'):
+                stayInAlphabet -= 26
+            finalLetter = chr(stayInAlphabet)
+        cipherText += finalLetter
+
+    return cipherText
+
+# Second method
+def move_ten(st):
+  return ''.join(chr(ord(char)+10) if i<'q' else chr(ord(i)-16) for char in st)
