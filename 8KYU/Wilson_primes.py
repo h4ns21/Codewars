7799d61a186e63cc6da2258e4617c58fac0d4674
H4ns <88755387+h4ns21@users.noreply.github.com>
2022-01-29 00:38:49 +0100

    x

--- 8KYU/Wilson_primes.py
+++ 8KYU/Wilson_primes.py
@@ -1,3 +1,10 @@
+#First method
+
 import math
 def am_i_wilson(n):
     return n > 1 and bool(n == 2 or (n % 2 and (math.factorial(n - 1) + 1) % n == 0))
+
+# Second method
+
+def am_i_wilson(n):
+    return n in (5, 13, 563)
