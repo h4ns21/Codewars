8da969fb382d537f097a79c2e04584570096d76c
H4ns <88755387+h4ns21@users.noreply.github.com>
2022-01-29 22:33:42 +0100

    x

--- 8KYU/Sum_Mixed_Array.py
+++ 8KYU/Sum_Mixed_Array.py
@@ -1,8 +1,20 @@
-def sum_mix(list):    
+# First method
+
+def sum_mix(arr):    
     total = 0
-    for element in list:
+    for element in arr:
     # checking whether its a number or not
-        if isinstance(element, int) or element.isdigit():
+        if isinstance(element, int) or element.isdigit() or str(element):
         # adding the element to the total
             total += int(element)
     return total
+
+# Second method
+
+def sum_mix(arr):
+    return sum(map(int, arr))
+
+# Third method
+
+def sum_mix(arr):
+    return sum(int(n) for n in arr)
