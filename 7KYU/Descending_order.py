491a0989a39f4c55043817fd60650f0148985548
H4ns <88755387+h4ns21@users.noreply.github.com>
2022-01-30 19:07:26 +0100

    x

--- 7KYU/Descending_order.py
+++ 7KYU/Descending_order.py
@@ -1,2 +1,9 @@
+# First method
+
 def descending_order(n):
     return int(''.join(map(str, sorted([i for i in str(n)], reverse=True))))
+
+# Second method
+
+def Descending_Order(n):
+    return int(''.join(sorted(str(n), reverse=True)))
