6923ad8402e684984662ac8766c65366ccd7e3eb
H4ns <88755387+h4ns21@users.noreply.github.com>
2022-02-06 19:43:36 +0100

    x

---
+++ 8KYU/Calculate_BMI.py
@@ -0,0 +1,18 @@
+# First method
+
+def bmi(weight, height):
+    x = weight/(height*height)
+    if x <= 18.5:
+        return "Underweight"
+    elif x <= 25.0:
+        return "Normal"
+    elif x <= 30.0:
+        return "Overweight"
+    elif x > 30:
+        return "Obese"
+        
+ # Second method
+ 
+ def bmi(weight, height):
+    b = weight / height ** 2
+    return ['Underweight', 'Normal', 'Overweight', 'Obese'][(b > 30) + (b > 25) + (b > 18.5)]
