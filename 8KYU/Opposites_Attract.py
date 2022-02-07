904e92335010855b52c71f0a55966b793098c681
H4ns <88755387+h4ns21@users.noreply.github.com>
2022-02-07 11:14:11 +0100

    x

---
+++ 8KYU/Opposites_Attract.py
@@ -0,0 +1,12 @@
+# First method
+
+def lovefunc(f1, f2):
+    if (f1 % 2 == 0) and (f2 % 2 != 0) or (f1 % 2 != 0) and (f2 % 2 == 0):
+        return True
+    else:
+        return False
+
+# Second method
+
+def lovefunc(f1, f2):
+    return (f1+f2)%2
